ddac0ce9497699a70bd546218e193f995cb1b8d2
Ranj2 <ranj.313185016@uhd.edu.iq>
2020-02-04 20:28:10 +0300

    Commited

---
+++ src/Runner.java
@@ -0,0 +1,12 @@
+public class Runner {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.delete_at(0);
+        list.delete_at(0);
+        list.show();
+    }
+}
